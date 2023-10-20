64d75e3549e70f469c9e052f203688c63b4a340a
Roberto Costa <you@example.com>
2023-10-20 16:10:16 -0300

    Subtotal & Total methods

--- src/main/java/com/robertocosta/course/entities/Order.java
+++ src/main/java/com/robertocosta/course/entities/Order.java
@@ -100,6 +100,15 @@ public class Order implements Serializable {
 		this.payment = payment;
 	}
 	
+	public Double getTotal() {
+//		double total = 0.00;
+//		for(OrderItem item : items) {
+//			total += item.getSubTotal();
+//		}
+//		return total;
+		return getItems().stream().mapToDouble(i -> i.getSubTotal()).sum();
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
